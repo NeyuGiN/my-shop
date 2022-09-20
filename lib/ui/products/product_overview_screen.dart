e078f658f459e28a574659943e8e5a4401b6126b
NeyuGiN <tangnguyen123.cm@gmail.com>
2022-09-20 14:57:39 +0700

    Them thong tin so san pham trong gio hang

--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/ui/cart/cart_screen.dart';
 import 'product_grid.dart';
 import '../shared/app_drawer.dart';
-
+import '../cart/cart_manager.dart';
+import 'top_right_badge.dart';
 enum FilterOptions { favorites , all }
 class ProductsOverviewScreen extends StatefulWidget{
   const ProductsOverviewScreen({super.key});
@@ -27,13 +28,16 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
     );
   }
   Widget buildShoppingCartIcon(){
-    return IconButton(
-      icon: const Icon(
-        Icons.shopping_cart,
+    return TopRightBadge(
+      data: CartManager().productCount,
+      child: IconButton(
+        icon: const Icon(
+          Icons.shopping_cart,
+        ),
+        onPressed: (){
+          Navigator.of(context).pushNamed(CartScreen.routeName);
+        },
       ),
-      onPressed: (){
-        Navigator.of(context).pushNamed(CartScreen.routeName);
-      },
     );
   }
   Widget buildProductFilterMenu(){
